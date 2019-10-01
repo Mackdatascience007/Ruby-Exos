497f6bd537b75bb8948986d2b042eb9b16fc5c26
cflament <flament.cha@gmail.com>
2019-10-01 16:20:03 +0200

    condition sur le 0 exo 18

--- exo_18.rb
+++ exo_18.rb
@@ -1,6 +1,11 @@
 emails = Array.new
 i = 01
-50.times do email = "super_user#{i}@thp.com"
+50.times do
+       if i<=9
+	       email = "super_user0#{i}@thp.com"
+       else
+	       email = "super_user#{i}@thp.com"
+       end
     emails << email
     i += 1
 end
